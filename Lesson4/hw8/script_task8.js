a624cead54d3772b0118d042c08df3124784fbcb
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-05-22 21:32:53 +0300

    fin task

---
+++ Lesson4/hw8/script_task8.js
@@ -0,0 +1,13 @@
+let list_array=[5,'text',true];
+let n=10;
+
+
+function list_print(list){
+
+    document.write(`<ul>`);
+    for(const list_arr of list)
+             {document.write(`<li>${list_arr}</li>`); }
+    document.write(`</ul>`);
+
+}
+list_print(list_array);
