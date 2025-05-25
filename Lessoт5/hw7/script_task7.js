163e0cf5038fbf41cc44a69ef3480d3f5ea7b96a
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-05-25 20:49:50 +0300

    fin task

--- Lessoт5/hw7/script_task7.js
+++ Lessoт5/hw7/script_task7.js
@@ -2,11 +2,11 @@ let list='Список';
 let n=10;
 
 
-function list_print(list,n){
+let listPrint=(list,n)=>{
 
     document.write(`<ul>`);
     for(let i=0;i<n;i++){document.write(`<li>${list}</li>`); }
     document.write(`</ul>`);
 
 }
-list_print(list,n);
+listPrint(list,n);
