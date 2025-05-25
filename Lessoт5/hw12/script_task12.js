38326d9aaf785b16b60176eee2bb2186a4f80f63
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-05-25 21:08:27 +0300

    fin task

--- Lessoт5/hw12/script_task12.js
+++ Lessoт5/hw12/script_task12.js
@@ -1,6 +1,6 @@
 let mas=[2,5,4,6,1,8,4,5,9];
 
-function swap(mas,index1,index2){
+let swap=(mas,index1,index2)=>{
     let r;
      r=mas[index1];
      mas[index1]=mas[index2];
--- Lessoт5/hw13/script_task13.js
+++ Lessoт5/hw13/script_task13.js
@@ -1,4 +1,3 @@
-
 let ExchangeRate=[
     {
         currency:'USD',
@@ -9,7 +8,7 @@ let ExchangeRate=[
         value:42,
     }
 ]
-function exchange(sumUAH,currencyValues,exchangeCurrency){
+let exchange=(sumUAH,currencyValues,exchangeCurrency)=>{
     if (exchangeCurrency==='USD'){
         return sumUAH/currencyValues[0].value;
     }
@@ -17,12 +16,8 @@ function exchange(sumUAH,currencyValues,exchangeCurrency){
         return sumUAH/currencyValues[1].value;
     }
 }
-let Rate;
-Rate=Math.round(exchange(10000,ExchangeRate,'USD')*100)/100;
-
+let Rate=Math.round(exchange(10000,ExchangeRate,'USD')*100)/100;
 document.write(`UAN=>USD ${Rate} <br>`);
-
 Rate=Math.round(exchange(10000,ExchangeRate,'EUR')*100)/100;
-
 document.write(`UAN=>EUR ${Rate}`);
 
