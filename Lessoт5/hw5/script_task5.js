868ad2fe70adba2286599fd39bf2edbaff54a7b0
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-05-25 20:47:36 +0300

    fin task

--- Lessoт5/hw5/script_task5.js
+++ Lessoт5/hw5/script_task5.js
@@ -1,7 +1,6 @@
-let text_='створити функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент'
+let text_='створити функцію, яка створює параграф з текстом та виводить його через document.write. ' +
+    'Текст задати через аргумент';
 
 
-function text_print(text){
-    document.write(`<p>${text}</p>`);
-}
-text_print(text_);
+let textPrint=(text)=>document.write(`<p>${text}</p>`);
+textPrint(text_);
