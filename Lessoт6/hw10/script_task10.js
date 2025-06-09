6434686265b598751ccc51fe55f52be4b35d73f2
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-06-09 19:06:38 +0300

    fin task

--- Lessoт6/hw10/script_task10.js
+++ Lessoт6/hw10/script_task10.js
@@ -1,13 +1,45 @@
-let mas=[2,5,4,6,1,8,4,5,9];
-let min;
-let minArray=(mas)=>{
-     let min=mas[0];
-    for(let number of mas) {
-        if (min > number) {
-            min = number;
+let cards=[];
+let value =[6,7,8,9,10,'ace','jack','queen','king'];
+let cardSuit=['spade', 'diamond','heart', 'clubs'];
+let color= ['red','black'];
+
+for(let valueCard of value){
+    for(let suit of cardSuit){
+        if((suit==='spade')||(suit==='clubs'))
+        {
+            cards.push({value:valueCard,cardSuit:suit,color:'black'});
+        }
+        if((suit==='diamond')||(suit==='heart'))
+        {
+            cards.push({value:valueCard,cardSuit:suit,color:'red'});
         }
     }
-    return min;
 }
-min=minArray(mas);
-console.log(min);
+//знайти піковий туз
+console.log(cards.filter(value=>{
+    if((value.value==='ace')&&(value.cardSuit==='spade'))
+    {
+        return value
+    }
+}));
+//всі шістки
+console.log(cards.filter(value=>{
+        return value.value===6
+}));
+//всі червоні карти
+console.log(cards.filter(value=>{
+    return value.color==='red'
+}));
+//всі буби
+console.log(cards.filter(value=>{
+    return value.cardSuit==='diamond'
+}));
+//всі трефи від 9 та більше
+let cardsClubs= cards.filter(user=>{
+    if((user.cardSuit==='clubs'))
+    {
+        return user
+    }
+});
+console.log(cardsClubs.slice(3,8))
+
