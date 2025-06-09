2e2434075e2b0fe86399543112e10f28eaba88c8
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-06-09 19:47:41 +0300

    fin task

--- Lessoт6/hw11/script_task11.js
+++ Lessoт6/hw11/script_task11.js
@@ -1,10 +1,33 @@
-let mas=[2,5,4,6,1,8,4,5,9];
+let cards=[];
+let value =[6,7,8,9,10,'ace','jack','queen','king'];
+let cardSuit=['spade', 'diamond','hearts', 'clubs'];
+let color= ['red','black'];
 
-let sumArray=(mas)=>{
-    let sum=0;
-     for(let number of mas) {
-        sum=sum+number;
+for(let valueCard of value){
+    for(let suit of cardSuit){
+        if((suit==='spade')||(suit==='clubs'))
+        {
+            cards.push({value:valueCard,cardSuit:suit,color:'black'});
+        }
+        if((suit==='diamond')||(suit==='hearts'))
+        {
+            cards.push({value:valueCard,cardSuit:suit,color:'red'});
+        }
     }
-    return sum;
 }
-console.log(sumArray(mas));
+let cardGroup=cards.reduce((accumulator,card)=>{
+    if(card.cardSuit==='spade'){
+        accumulator.spades.push(card);
+    }
+    if(card.cardSuit==='diamond'){
+        accumulator.diamonds.push(card);
+    }
+    if(card.cardSuit==='hearts'){
+        accumulator.hearts.push(card);
+    }
+    if(card.cardSuit==='clubs'){
+        accumulator.clubs.push(card);
+    }
+    return accumulator;
+},{spades:[],diamonds:[],hearts:[],clubs:[]});
+console.log(cardGroup);
