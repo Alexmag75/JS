25ce876218519250bb1294e25bacdd133abe9cc4
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-11-12 17:05:32 +0200

    fin task

--- Lesson9/hw2/script_task2.js
+++ Lesson9/hw2/script_task2.js
@@ -1,34 +1,13 @@
-let user1={
-    id:1,
-    name:'vasia',
-    email:'vasia@gmail.com',
-    message1: function (){
-        console.log('Привет')
-    },
-    message2: function (){
-        console.log('Бывай')
-    }
+let arr=['Main','Products','About us','Contacts'];
+
+const menu=document.createElement("ul");
+
+for(let i=0;i<arr.length;i++){
+    const li = document.createElement("li");
+    li.innerText=arr[i];
+    menu.appendChild(li);
 }
+document.body.appendChild(menu);
 
-function copyUser(user){
-    if(user){
-        let masFunction=[];
-        for(const key in user){
-            if(typeof user[key]==='function'){
-                const functionCopy=user[key].bind({});
-                masFunction.push({functionCopy,key});
-            }
-        }
-        const userCopy=JSON.parse(JSON.stringify(user));
-        for (const func of masFunction){
-            userCopy[func.key] = func.functionCopy;
-        }
-        return userCopy;
-    }
-}
 
-user2=copyUser(user1);
-
-console.log(user1);
-console.log(user2);
 
