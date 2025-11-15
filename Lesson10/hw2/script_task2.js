b01cb852cbb2c8caae8f22905e78fc60342946bf
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-11-15 13:12:56 +0200

    fin task

--- Lesson10/hw2/script_task2.js
+++ Lesson10/hw2/script_task2.js
@@ -1,13 +1,10 @@
-let arr=['Main','Products','About us','Contacts'];
 
-const menu=document.createElement("ul");
-
-for(let i=0;i<arr.length;i++){
-    const li = document.createElement("li");
-    li.innerText=arr[i];
-    menu.appendChild(li);
+const btn=document.getElementsByTagName('button')[0];
+btn.onclick = function(){
+    const age=document.getElementById('age');
+    if(age.value<18) {
+        alert('Ваш возраст не подходит для данного контента')
+    }
 }
-document.body.appendChild(menu);
-
 
 
